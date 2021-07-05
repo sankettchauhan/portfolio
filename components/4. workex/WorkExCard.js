f982caf2cdb3a4610cc12efee563721bf42666a0
sankettchauhan <sanket.chauhan4@gmail.com>
2021-07-05 23:33:15 +0530

    tried something new

--- components/4. workex/WorkExCard.js
+++ components/4. workex/WorkExCard.js
@@ -3,25 +3,25 @@ import styles from "./WorkExCard.module.css";
 export default function WorkEx({ date, title, logo, company, technologies }) {
   return (
     <article className={styles.card}>
-      <header className={styles.cardHeader}>
+      <header className={`${styles.cardHeader} ${styles.content}`}>
         <p>{date}</p>
         <h2>{title}</h2>
       </header>
 
-      <div className={styles.cardAuthor}>
+      <div className={`${styles.cardAuthor} ${styles.content}`}>
         <div className={styles.authorAvatar} href="#">
           <img src={logo} />
         </div>
         <svg className={styles.halfCircle} viewBox="0 0 106 57">
           <path d="M102 4c0 27.1-21.9 49-49 49S4 31.1 4 4"></path>
         </svg>
-
         <div className={styles.authorName}>
           <div className={styles.authorNamePrefix}>Company</div>
           {company}
         </div>
       </div>
-      <div className={styles.tags}>
+
+      <div className={`${styles.tags} ${styles.content}`}>
         {technologies.map((item, index) => (
           <span key={`Tech-${index + 1}`}>{item}</span>
         ))}
