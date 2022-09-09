b9d785ab205aff3189a6c5df66d173839b99281c
sanket chauhan <sanket.chauhan4@gmail.com>
2022-09-10 00:52:15 +0530

    add tags to portfolio

--- components/3. projects/Project.js
+++ components/3. projects/Project.js
@@ -1,10 +1,44 @@
 import styles from "./Project.module.css";
 import ProjectCard from "./ProjectCard";
 import PROJECTS from "../../content/projects.json";
+import { useState } from "react";
+
+const TAGS = new Set([
+  "fullstack",
+  "design",
+  "react",
+  "node",
+  "tailwind",
+  "api",
+  "backend",
+  "bootstrap",
+  "css",
+  "express",
+  "firebase",
+  "frontend",
+  "game",
+  "html",
+  "material ui",
+  "mongodb",
+  "pug",
+]);
 
 export default function Projects() {
   // remove the react chat app project
-  PROJECTS.pop();
+  const [selectedTags, setselectedTags] = useState([]);
+  console.log(
+    PROJECTS.filter(({ hiddenTags }) => hiddenTags.some((e) => TAGS.has(e)))
+  );
+
+  const handleClick = (clickedTag) => {
+    const selectedTagsSet = new Set(selectedTags);
+    // tag is present in selected tags -> remove
+    if (selectedTagsSet.has(clickedTag))
+      setselectedTags((s) => s.filter((e) => e !== clickedTag));
+    // tag is not present -> add
+    else setselectedTags((s) => [...s, clickedTag]);
+  };
+
   return (
     <div id="projects" className={styles.projectSection}>
       <div className={styles.title}>
@@ -12,9 +46,32 @@ export default function Projects() {
         <div className="scroll">Click on the project to view it!</div>
       </div>
       <div className={styles.cardContainer}>
-        {PROJECTS.map((item, index) => (
-          <ProjectCard key={`Project-card-${index + 1}`} {...item} />
-        ))}
+        <div className={styles.tagContainer}>
+          {Array.from(TAGS).map((tag, index) => {
+            const selectedTagsSet = new Set(selectedTags);
+            return (
+              <button
+                key={`tag-${index}`}
+                className={`${styles.tag} ${
+                  selectedTagsSet.has(tag) ? styles.tagActive : ""
+                }`}
+                onClick={() => handleClick(tag)}
+              >
+                #{tag}
+              </button>
+            );
+          })}
+        </div>
+        {selectedTags.length === 0
+          ? PROJECTS.map((item, index) => (
+              <ProjectCard key={`Project-card-${index + 1}`} {...item} />
+            ))
+          : PROJECTS.filter(({ hiddenTags }) => {
+              const selectedTagsSet = new Set(selectedTags);
+              return hiddenTags.some((e) => selectedTagsSet.has(e));
+            }).map((item, index) => (
+              <ProjectCard key={`Project-card-${index + 1}`} {...item} />
+            ))}
       </div>
     </div>
   );
---
+++ content/deleted-projects.js
@@ -0,0 +1,11 @@
+[
+  {
+    title: "React superchat app",
+    image: "/assets/images/project/react-superchat.webp",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    href: "https://superchat-using-react-firebase.vercel.app/",
+    tags: ["react", "firebase", "material-ui"],
+    hiddenTags: ["react", "firebase", "design", "frontend"],
+  },
+];
